b9212afd076517682ece67be70aebf1560d65e53
Pritom Paul <pritompaul1920@gmail.com>
2023-12-08 01:48:44 +0600

    ...

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -86,11 +86,11 @@ export default function Home({ setSelectedIndex }: Props) {
                 justifyContent="center"
               >
                 <a
-                  // href="https://drive.google.com/file/d/1OwEK4U5cyRwiAQcp6s3NAjRdxaD0kdmd/view?usp=sharing"
-                  href="/Assets/CV_Pritom_Paul.pdf"
+                  href="https://drive.google.com/file/d/1OwEK4U5cyRwiAQcp6s3NAjRdxaD0kdmd/view?usp=sharing"
+                  // href="/Assets/CV_Pritom_Paul.pdf"
                   target="_blank"
                   rel="noopener noreferrer"
-                  download={"CV_Pritom_Paul.pdf"}
+                  // download={"CV_Pritom_Paul.pdf"}
                 >
                   <img
                     width="145"
