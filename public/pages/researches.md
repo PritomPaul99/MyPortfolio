0fe58108aa42967c95aa972406037d7b6100cba1
Pritom Paul <pritompaul1920@gmail.com>
2023-12-10 21:01:54 +0600

    created researches.md

---
+++ public/pages/researches.md
@@ -0,0 +1,7 @@
+# 🧮  Researches
+
+## 👉 Bangla Handwritten Text Recognition Using Image-To-Sequence Extraction `(Research)`
+
+The aim of my research is to recognize handwritten Bangla words utilizing Sequence-to-Sequence method that supposed to be superior than other text recognition techniques.
+
+- **Status 🕰️:** Ongoing.
--- src/app/pages/pages.ts
+++ src/app/pages/pages.ts
@@ -3,8 +3,9 @@ export const pages = [
   { index: 1, name: 'skills.md', route: '/skills', visible: true },
   { index: 2, name: 'experience.md', route: '/experience', visible: true },
   { index: 3, name: 'education.md', route: '/education', visible: true },
-  { index: 4, name: 'projects_&_researches.md', route: '/projects_&_researches', visible: true },
-  { index: 5, name: 'certificates.md', route: '/certificates', visible: true },
-  { index: 6, name: 'accomplishments.md', route: '/accomplishments', visible: true },  
-  { index: 7, name: 'docs.md', route: '/docs', visible: false },  
+  { index: 4, name: 'projects.md', route: '/projects', visible: true },
+  { index: 5, name: 'researches.md', route: '/researches', visible: true },
+  { index: 6, name: 'certificates.md', route: '/certificates', visible: true },
+  { index: 7, name: 'accomplishments.md', route: '/accomplishments', visible: true },  
+  { index: 8, name: 'docs.md', route: '/docs', visible: false },  
 ];
